ad6a933b476d5ae4293e8a51129033e6f6507d8f
lecha_florencia <flecharegueira@gmail.com>
2022-11-15 17:00:43 -0300

    Arreglé componente posteos

--- evaluacion-noviembre-22-main/src/components/Posteos.js
+++ evaluacion-noviembre-22-main/src/components/Posteos.js
@@ -4,10 +4,12 @@ import data from "./data.json"
 
 export function Posteos({ handleAddTotalLike }) {
 
+  const post = data.map()
+
   return (
     <div className='container'>
-      {data.map(post => <Post key={post.id} handleAddTotalLike={handleAddTotalLike} post={post} />)}
+      <Post post={post} key={post.id} handleAddTotalLike={handleAddTotalLike}/>
     </div>
   )
-  
+
 }
